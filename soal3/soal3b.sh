58064b2fb23c3c281c60e43fd92d775b4c079c48
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-03-24 22:55:27 +0700

    Add jawaban no. 3b - Sudah benar :)

---
+++ soal3/soal3b.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Masuk ke folder repo dulu
+BASEDIR=$(dirname "$0")
+echo "Masuk ke $BASEDIR"
+cd "$BASEDIR"
+
+# Eksekusi yang 3a
+bash ./soal3a.sh
+
+# Bikin folder, lalu dipindah ke folder itu
+current_date=$(date +"%d-%m-%Y")
+mkdir "$current_date"
+mv ./Koleksi_* "./$current_date/"
+mv ./Foto.log "./$current_date/"
+
+# Sudah
+echo "Moved to $current_date"
