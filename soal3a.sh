d7c6b437b57bfcedb3564b26c3b21e2b0653d0ca
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-03-24 21:45:14 +0700

    Upload soal no. 3a - Sudah benar outputnya - Diberi .gitignore agar file untuk debug tidak terupload

---
+++ soal3a.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Menentukan nama file
+get_file_name_result=""
+get_file_name() {
+    if [ $1 -lt 10 ]
+    then
+        get_file_name_result="Koleksi_0$1"
+    else
+        get_file_name_result="Koleksi_$1"
+    fi
+}
+
+# Iterasi sampai 23
+file_ke=1
+while [ "$file_ke" -le 23 ]
+do
+    # Tentukan filename dulu
+    get_file_name "$file_ke"
+    filename=$get_file_name_result
+    wget -O "$filename" -a Foto.log https://loremflickr.com/320/240/kitten
+
+    # Cari, ada yang sama atau tidak
+    is_same=0
+    for((i=1; i < $file_ke; i++))
+    do
+        get_file_name "$i"
+        cmp_filename=$get_file_name_result
+        cmp_result=$(cmp "./$cmp_filename" "./$filename")
+        cmp_exit=$?
+        if [ $cmp_exit -eq 0 ]
+        then
+            is_same=1
+            break
+        fi
+    done
+
+    # Lakukan sesuai soal :v
+    if [ $is_same -eq 1 ]
+    then
+        echo "$filename: Hasil download sama, hapus dan ulang."
+        rm "./$filename"
+    else
+        echo "$filename: Hasil download berbeda, lanjut."
+        file_ke=$(($file_ke + 1)) 
+    fi
+done
+
+# Done
+echo "Sudah terdownload semua"
